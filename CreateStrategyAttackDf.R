18688582302550a08cb61e9cdd245f694e1b06c3
Jonathan Bourne <cheg_a_leg@hotmail.com>
2019-02-06 09:02:21 +0000

    Clean UP Create Strategy Attack
    made the function much smaller using a helper function. It is now much easier to read

--- CreateStrategyAttackDf.R
+++ CreateStrategyAttackDf.R
@@ -3,34 +3,30 @@ CreateStrategyAttackDf <- function(g){
   #using various different dataframes.
   #Maybe this should be put in the PowerGridNetworking Folder?
   
-  ElecCen1<- ElectricalCentrality(g)
-  #degree
-  StratAttackVect <- c(as_data_frame(g, what = "vertices") %>% 
-                         as_tibble %>%
-                         mutate(metric = degree(g)) %>%
-                         arrange(desc(metric)) %>% .$name,
-                       #entropic degree line limit
-                       as_data_frame(g, what = "vertices") %>%
-                         mutate(metric = EntropicDegree(g, Scale = TRUE)) %>%
-                         arrange(desc(metric)) %>% .$name,
-                       #entropic degree powerflow
-                       as_data_frame(g, what = "vertices") %>%
-                         mutate(metric = EntropicDegree(g, value = "PowerFlow", Scale = TRUE)) %>%
-                         arrange(desc(metric)) %>% .$name,
-                       
-                       #Electrical centrality
-                       ElecCen1$NodeEC %>% arrange(-NodeEC) %>% .$Bus.Name,
-                       
-                       #centrality
-                       as_data_frame(g, what = "vertices") %>%
-                         mutate(metric = betweenness(g)) %>%
-                         arrange(desc(metric)) %>% .$name
-  ) %>% matrix(., nrow = 5, byrow = T) %>%
-    as_tibble %>%
-    mutate(SimulationID = paste0("Simulation_ID_",1:5)) %>%
-    select(SimulationID, everything()) %>%
-    setNames(c("SimulationID", paste0("Target", 1:vcount(g))))
+  AttackOrderNames <- function(g, .fun, ...){
+    #Given a function that outputs a node score, this function returns the names of the nodes in descending order,
+    NodeDf <- as_data_frame(g, what = "vertices") %>% 
+      as_tibble %>%
+      mutate(metric = .fun(g, ...)) %>%
+      arrange(desc(metric)) %>% .$name
+    
+    return(NodeDf)
+    
+  }
   
+  ElecCen1<- ElectricalCentrality(g)
+  
+   StratAttackVect <-  c(AttackOrderNames(g, degree),
+                         AttackOrderNames(g, EntropicDegree, Scale = TRUE),
+                         AttackOrderNames(g, EntropicDegree,  value = "PowerFlow", Scale = TRUE),
+                         ElecCen1$NodeEC %>% arrange(-NodeEC) %>% .$Bus.Name,
+                         AttackOrderNames(g, betweenness)) %>% 
+   matrix(., nrow = 5, byrow = T) %>%
+   as_tibble %>%
+   mutate(SimulationID = paste0("Simulation_ID_",1:5)) %>%
+   select(SimulationID, everything()) %>%
+   setNames(c("SimulationID", paste0("Target", 1:vcount(g))))
+
   return(StratAttackVect)
   
 }
